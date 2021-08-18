13dfbbbe89035dd8fb3eb3061bad42da02effae9
minervamadrigal <minervamadrigal@protonmail.com>
2021-08-18 14:44:38 -0500

    portfolio div responsive

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const menuBtn = document.querySelector('.mobtrigger');
+const dropdown = document.querySelector('.dropdown');
 let menuOpen = false;
-
-console.log(menuBtn);
+let dropdownOpen = false;
 menuBtn.addEventListener('click', () =>{
     if(!menuOpen){
         menuBtn.classList.add('open');
         menuOpen = true;
+        dropdown.classList.add('open');
+        dropdownOpen = true;
     }else{
         menuBtn.classList.remove('open');
         menuOpen = false;
+        dropdown.classList.remove('open');
+        dropdownOpen = false;
     }
 } );
